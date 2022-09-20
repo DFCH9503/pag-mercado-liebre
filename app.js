507faed85f0d8dff613ec1e612aec1a1dff0e52b
DFCH9503 <dafechaparro@hotmail.com>
2022-09-19 20:42:03 -0500

    primera-pá-mercado-liebre

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express');
+const path=require('path');
+
+const app=express();
+const publicPath=path.join(__dirname,'/public');
+
+app.use(express.static(publicPath));
+
+
+app.listen(3000, ()=>{
+    console.log('Servidor OK en puerto 3000')
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/home.html'))
+});
