983dfbdd885952a6d1152864e6a2e1add07a7281
DFCH9503 <dafechaparro@hotmail.com>
2022-10-03 21:58:59 -0500

    añadiendo formularios-revisar

--- app.js
+++ app.js
@@ -14,3 +14,11 @@ app.listen(3000, ()=>{
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 });
+
+app.get('/register',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+});
+
+app.get('/login',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+});
