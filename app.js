02dad00196823c100a846c452446133c3880f602
DFCH9503 <dafechaparro@hotmail.com>
2022-10-07 16:57:17 -0500

    deploy render - falta :hover descripciones

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const publicPath=path.join(__dirname,'/public');
 app.use(express.static(publicPath));
 
 
-app.listen(3000, ()=>{
-    console.log('Servidor OK en','http://localhost:3000')
-});
+// app.listen(3000, ()=>{
+//     console.log('Servidor OK en','http://localhost:3000')
+// });
+
+const port=process.env.PORT || 3000;
+app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'));
+
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
