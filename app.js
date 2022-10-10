d26378f2b7ba671758e0b6aea7cf012ce132ba9e
DFCH9503 <dafechaparro@hotmail.com>
2022-10-10 13:02:37 -0500

    mercado-liebre-OK

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.static(publicPath));
 // });
 
 const port=process.env.PORT || 3000;
-app.listen(port,()=>console.log('Servidor corriendo en el puerto ${port}'));
+app.listen(port,()=>console.log(`Servidor corriendo en el puerto ${port}`));
 
 
 app.get('/',(req,res)=>{
